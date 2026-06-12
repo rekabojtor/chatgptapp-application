15628ba24a0d46e7fffa9ede3bf3c4871e9ee5ef
s600572 <reka.bojtor@student.htw-berlin.de>
2026-06-12 07:59:08 +0200

    init

--- main.py
+++ main.py
@@ -36,4 +36,4 @@ async def word_count(text: str) -> dict:
 
 
 if __name__ == "__main__":
-    mcp.run(transport="sse", host="0.0.0.0", port=8000)
+    mcp.run(transport="http", host="127.0.0.1", port=8000)
